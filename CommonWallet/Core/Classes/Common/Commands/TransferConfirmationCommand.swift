86a13951e060d941191582599301392158b9b2a4
Emkil <emkil.russel@gmail.com>
2020-10-14 02:36:37 +0300

    add opportunity to decorate confirmation screen presentation

---
+++ CommonWallet/Core/Classes/Common/Commands/TransferConfirmationCommand.swift
@@ -0,0 +1,30 @@
+/**
+* Copyright Soramitsu Co., Ltd. All Rights Reserved.
+* SPDX-License-Identifier: GPL-3.0
+*/
+
+
+import Foundation
+
+final class TransferConfirmationCommand: WalletPresentationCommandProtocol {
+    let payload: ConfirmationPayload
+    let resolver: ResolverProtocol
+
+    var presentationStyle: WalletPresentationStyle = .push(hidesBottomBar: true)
+    var animated: Bool = true
+
+    init(payload: ConfirmationPayload, resolver: ResolverProtocol) {
+        self.payload = payload
+        self.resolver = resolver
+    }
+
+    func execute() throws {
+        guard let confirmationView = TransferConfirmationAssembly.assembleView(with: resolver,
+                                                                               payload: payload),
+            let navigation = resolver.navigation else {
+            return
+        }
+
+        present(view: confirmationView.controller, in: navigation, animated: animated)
+    }
+}
