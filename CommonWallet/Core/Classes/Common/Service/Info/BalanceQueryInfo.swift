6b95207ae2b6b304503faaa84ef2de344c21174c
Aleksandr Malina <a.malina@fbs.com>
2021-03-04 09:56:52 +0300

    Review BalanceQueryInfo

--- CommonWallet/Core/Classes/Common/Service/Info/BalanceQueryInfo.swift
+++ CommonWallet/Core/Classes/Common/Service/Info/BalanceQueryInfo.swift
@@ -6,8 +6,8 @@
 import Foundation
 
 public struct BalanceQueryInfo: Codable {
-    public var assets: [String]
-    public var query: Data
+    public let assets: [String]
+    public let query: Data
 
     public init(assets: [String], query: Data) {
         self.assets = assets
