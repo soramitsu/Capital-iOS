d584da986485019770f0882f3353861bad5e371e
Aleksandr Malina <a.malina@fbs.com>
2021-03-04 08:59:24 +0300

    Review DefaultEmptyStateDataSource

--- Example/CommonWallet/EmptyState/DefaultEmptyStateDataSource.swift
+++ Example/CommonWallet/EmptyState/DefaultEmptyStateDataSource.swift
@@ -6,22 +6,17 @@
 import UIKit
 import SoraUI
 
-final class DefaultEmptyStateDataSource {
-    var titleForEmptyState: String?
-    var imageForEmptyState: UIImage?
-    var titleColorForEmptyState: UIColor? = UIColor(white: 137.0 / 255.0, alpha: 1.0)
-    var titleFontForEmptyState: UIFont? = UIFont(name: "HelveticaNeue", size: 12.0)
-    var verticalSpacingForEmptyState: CGFloat? = 16.0
-    var trimStrategyForEmptyState: EmptyStateView.TrimStrategy = .none
+final class DefaultEmptyStateDataSource: EmptyStateDataSource {
+    let viewForEmptyState: UIView? = nil
+    let titleForEmptyState: String?
+    let imageForEmptyState: UIImage?
+    let titleColorForEmptyState: UIColor? = UIColor(white: 137.0 / 255.0, alpha: 1.0)
+    let titleFontForEmptyState: UIFont? = UIFont(name: "HelveticaNeue", size: 12.0)
+    let verticalSpacingForEmptyState: CGFloat? = 16.0
+    let trimStrategyForEmptyState = EmptyStateView.TrimStrategy.none
 
     init(title: String, image: UIImage? = nil) {
         self.titleForEmptyState = title
         self.imageForEmptyState = image
-    }
-}
-
-extension DefaultEmptyStateDataSource: EmptyStateDataSource {
-    var viewForEmptyState: UIView? {
-        return nil
     }
 }
