99db0ee4c2875df34b7f16db865ecd57160881b3
Aleksandr Malina <a.malina@fbs.com>
2021-03-04 10:04:17 +0300

    Remove unused property

--- CommonWallet/Core/Classes/Common/Model/WalletHistoryRequest.swift
+++ CommonWallet/Core/Classes/Common/Model/WalletHistoryRequest.swift
@@ -9,7 +9,6 @@ import Foundation
 public struct WalletHistoryRequest: Codable, Equatable {
     
     public var assets: [String]?
-    public var filter: String?
     public var fromDate: Date?
     public var toDate: Date?
     public var type: String?
