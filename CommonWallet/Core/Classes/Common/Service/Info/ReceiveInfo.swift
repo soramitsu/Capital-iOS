cf1ce0a62f910bdcd40d849dc920fbeb664efceb
Aleksandr Malina <a.malina@fbs.com>
2021-03-04 09:59:34 +0300

    Review ReceiveInfo

--- CommonWallet/Core/Classes/Common/Service/Info/ReceiveInfo.swift
+++ CommonWallet/Core/Classes/Common/Service/Info/ReceiveInfo.swift
@@ -8,10 +8,10 @@ import Foundation
 
 public struct ReceiveInfo: Codable, Equatable {
 
-    public var accountId: String
-    public var assetId: String?
+    public let accountId: String
+    public let assetId: String?
     public var amount: AmountDecimal?
-    public var details: String?
+    public let details: String?
 
     public init(accountId: String, assetId: String?, amount: AmountDecimal?, details: String?) {
         self.accountId = accountId
