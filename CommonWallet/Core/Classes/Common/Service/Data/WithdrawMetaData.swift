45662aca93595096ac1a14f7d205b0ef0fb55fe9
Aleksandr Malina <a.malina@fbs.com>
2021-03-04 09:55:12 +0300

    Review WithdrawMetaData

--- CommonWallet/Core/Classes/Common/Service/Data/WithdrawMetaData.swift
+++ CommonWallet/Core/Classes/Common/Service/Data/WithdrawMetaData.swift
@@ -6,8 +6,8 @@
 import Foundation
 
 public struct WithdrawMetaData: Codable, Equatable {
-    public var providerAccountId: String
-    public var feeDescriptions: [FeeDescription]
+    public let providerAccountId: String
+    public let feeDescriptions: [FeeDescription]
 
     public init(providerAccountId: String, feeDescriptions: [FeeDescription]) {
         self.providerAccountId = providerAccountId
